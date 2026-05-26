87facb618515f06890fb87776af140d23786b248
MarcosMS83 <mms_ee@hotmail.com>
2026-05-26 09:44:15 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,26 @@
 import asyncio
+import os
+
+from telegram import Bot
+from dotenv import load_dotenv
+
+# =====================================================
+# ENV
+# =====================================================
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv(
+    "TELEGRAM_BOT_TOKEN"
+)
+
+CHAT_ID = os.getenv(
+    "TELEGRAM_CHAT_ID"
+)
+
+bot = Bot(
+    token=BOT_TOKEN
+)
 
 # =====================================================
 # LOOP BOT
@@ -12,8 +34,28 @@ async def loop_bot():
 
     while True:
 
-        print(
-            "BOT RODANDO..."
-        )
+        try:
 
-        await asyncio.sleep(30)
+            print(
+                "ENVIANDO TESTE TELEGRAM..."
+            )
+
+            await bot.send_message(
+
+                chat_id=CHAT_ID,
+
+                text="✅ BOT ONLINE"
+            )
+
+            print(
+                "MENSAGEM ENVIADA"
+            )
+
+        except Exception as e:
+
+            print(
+                "ERRO TELEGRAM:",
+                e
+            )
+
+        await asyncio.sleep(60)
